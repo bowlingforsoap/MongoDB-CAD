8a6564b8aaa2208886d95b848f8e8c8e149926a9
Vadym <Vadym@hp>
2015-06-10 10:02:47 +0300

    minor changes

--- Evaluation/src/main/java/ua/kharkiv/mongorigami/Evaluation/view/MainFrame.java
+++ Evaluation/src/main/java/ua/kharkiv/mongorigami/Evaluation/view/MainFrame.java
@@ -22,11 +22,18 @@ import javax.swing.border.TitledBorder;
 import javax.swing.tree.DefaultTreeModel;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.JTable;
+import javax.swing.border.BevelBorder;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
 
 public class MainFrame {
 
 	private JFrame frame;
 	private JTable table;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -56,7 +63,7 @@ public class MainFrame {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 710, 449);
+		frame.setBounds(100, 100, 749, 551);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(new BorderLayout(0, 0));
 		
@@ -69,10 +76,11 @@ public class MainFrame {
 				}
 			}
 		));
-		frame.getContentPane().add(tree, BorderLayout.WEST);
+		frame.getContentPane().add(new JScrollPane(tree), BorderLayout.WEST);
 		
 		JPanel panel = new JPanel();
-		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		JScrollPane scrollPane_1 = new JScrollPane(panel);
+		frame.getContentPane().add(scrollPane_1, BorderLayout.CENTER);
 		panel.setLayout(null);
 		
 		JPanel panel_1 = new JPanel();
@@ -91,19 +99,87 @@ public class MainFrame {
 		comboBox.addItem("aggregate");
 		
 		JButton btnAdd = new JButton("Add");
-		btnAdd.setBounds(150, 23, 51, 23);
+		btnAdd.setBounds(150, 23, 70, 23);
 		panel_1.add(btnAdd);
 		
 		JPanel panel_2 = new JPanel();
 		panel_2.setLayout(null);
 		panel_2.setBorder(new TitledBorder("Insert ass."));
-		panel_2.setBounds(10, 97, 586, 65);
-		panel.add(panel_2);
+		panel_2.setBounds(10, 97, 586, 217);
+		scrollPane.setSize(525, 116);
+		scrollPane.setLocation(21, 74);
+		scrollPane.setViewportBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		panel_2.add(scrollPane);
+		
+		textField = new JTextField();
+		textField.setToolTipText("Insert a reference name of the assessment herre");
+		textField.setBounds(66, 29, 166, 20);
+		panel_2.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblName = new JLabel("Name:");
+		lblName.setBounds(21, 32, 46, 14);
+		panel_2.add(lblName);
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setLayout(null);
+		panel_3.setBorder(new TitledBorder("Insert ass."));
+		panel_3.setBounds(10, 97, 586, 336);
+		panel.add(panel_3);
+		
+		textField_1 = new JTextField();
+		textField_1.setToolTipText("Insert a reference name of the assessment herre");
+		textField_1.setColumns(10);
+		textField_1.setBounds(66, 29, 166, 20);
+		panel_3.add(textField_1);
+		
+		JLabel label = new JLabel("Name:");
+		label.setBounds(21, 32, 46, 14);
+		panel_3.add(label);
+		
+		JPanel panel_4 = new JPanel();
+		panel_4.setBounds(21, 76, 525, 157);
+		panel_3.add(panel_4);
+		panel_4.setLayout(null);
+		//panel.add(panel_2);
 		
 		table = new JTable();
-		table.setBounds(82, 22, 187, 18);
-		panel_2.add(table);
+		table.setRowSelectionAllowed(false);
+		table.setColumnSelectionAllowed(true);
+		table.setCellSelectionEnabled(true);
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{"policy._id", Boolean.FALSE},
+				{"policy.state", Boolean.FALSE},
+				{"car._id", Boolean.FALSE},
+				{"car.model", Boolean.FALSE},
+				{"car.mileage", Boolean.FALSE},
+				{"car.plateNum", Boolean.FALSE},
+				{"car.company", Boolean.FALSE},
+				{"car.buildYear", Boolean.FALSE},
+			},
+			new String[] {
+				"Filed", "Select"
+			}
+		) {
+			Class[] columnTypes = new Class[] {
+				Object.class, Boolean.class
+			};
+			public Class getColumnClass(int columnIndex) {
+				return columnTypes[columnIndex];
+			}
+		});
+		table.getColumnModel().getColumn(0).setPreferredWidth(113);
+		table.getColumnModel().getColumn(1).setPreferredWidth(42);
+		table.setFillsViewportHeight(true);
+		table.setPreferredScrollableViewportSize(table.getPreferredSize());
+		JScrollPane scrollPane = new JScrollPane(table);
 		
+		JScrollPane scrollPane_2 = new JScrollPane(table);
+		scrollPane_2.setBounds(10, 22, 525, 116);
+		panel_4.add(scrollPane_2);
+		scrollPane_2.setViewportBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+
 		JMenuBar menuBar = new JMenuBar();
 		frame.setJMenuBar(menuBar);
 		
